e5f86e9f1fe1fa0eca4021ff7ca1f1ae618d9b80
Qi Ren <ren.qi1@northeastern.edu>
2024-04-24 19:13:40 -0400

    cors

--- src/pages/api/account/signin.js
+++ src/pages/api/account/signin.js
@@ -1,10 +1,13 @@
 import {MEG_NOT_SUPPORTED} from "@/commons/Constants";
 import dbConnect from "@/helper/backend/database";
 import {checkAuth, checkUserLogin} from "@/helper/backend/user";
+import {corsMiddleware} from "@/helper/backend/auth";
 
 export default async function handler(
     req, res
 ) {
+    await corsMiddleware(req, res);
+
     const method = req.method;
 
     await dbConnect();
