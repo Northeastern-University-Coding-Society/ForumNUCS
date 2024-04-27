bb3d441efec358524a998ff7575ca1c9b45e8f7c
Qi Ren <ren.qi1@northeastern.edu>
2024-04-27 08:31:51 -0400

    sorting

--- src/pages/api/post/all.js
+++ src/pages/api/post/all.js
@@ -10,12 +10,24 @@ export default async function handler(
     // no check for user identity
     await corsMiddleware(req, res);
     const method = req.method;
+    const params = req.query;
+    console.log(params)
     if (method === 'GET') {
         await dbConnect();
-        const all = await post.find({
-            status: {$ne: 'deleted'}
-        }, {})
-        return res.status(200).json(all);
+        if (params.sort) {
+            const sorter = params.sort === 'name' ? 'date' : 'title';
+            const all = await post.find({
+                status: {$ne: 'deleted'}
+            }, {}).sort({
+                [sorter]: 1
+            })
+            return res.status(200).json(all);
+        } else {
+            const all = await post.find({
+                status: {$ne: 'deleted'}
+            }, {})
+            return res.status(200).json(all);
+        }
     }
     res.status(500).send(MEG_NOT_SUPPORTED);
 }
